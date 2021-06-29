8584cc7dd3fa01ecdebbdc5c80c255b326846c03
ManethWijetunga <2019cs194@stu.ucsc.lk>
2021-06-29 10:55:15 +0530

    initialisation of visitor's page

--- Codes/public/js/faq.js
+++ Codes/public/js/faq.js
@@ -1,15 +1,19 @@
-var acc= document.getElementsByClassName('accordion');
+
+var acc = document.getElementsByClassName("accordion");
 var i;
-var len=acc.length;
-for (i=0; i<len; i++){
-    acc[i].addEventListener('click', function(){
-        this.classList.toggle('active');
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight){
-            panel.style.maxHeight =null;
-        }
-        else{
-            panel.style.maxHeight= panel.scrollHeight + 'px'
-        }
-    })
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
 }
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello World";
+  }
