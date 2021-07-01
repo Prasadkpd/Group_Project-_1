9b67a955481a17eecfbc81dc7369970e72b53187
ManethWijetunga <2019cs194@stu.ucsc.lk>
2021-07-01 20:08:43 +0530

    Done with faqs of customer

--- Codes/public/js/app.js
+++ Codes/public/js/app.js
@@ -14,3 +14,19 @@ function showSlides() {
   setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
 
+//Accrodion function
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
