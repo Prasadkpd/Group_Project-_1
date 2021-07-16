5aa3d9d9860c147b53860d3262bdcbb9707b03fa
ManethWijetunga <2019cs194@stu.ucsc.lk>
2021-07-16 07:53:36 +0530

    sp-arena section partly completed

--- Codes/public/js/app.js
+++ Codes/public/js/app.js
@@ -157,6 +157,43 @@ document.querySelector('#next-year').onclick = () => {
 }
 
 
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+// function myFunction1() {
+//   document.getElementById("myDropdown1").classList.toggle("show1");
+// }
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//   if (!event.target.matches('.dropbtn1')) {
+//     var dropdowns = document.getElementsByClassName("dropdown-content1");
+//     var i;
+//     for (i = 0; i < dropdowns.length; i++) {
+//       var openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show1')) {
+//         openDropdown.classList.remove('show1');
+//       }
+//     }
+//   }
+// }
 
 
 
