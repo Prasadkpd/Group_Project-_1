b3bc52ae5e1e892c49396037b5ed6f4b16d4ff36
Prasadkpd <2019cs078@stu.ucsc.lk>
2021-08-09 22:41:45 +0530

    Architecture folder adding

---
+++ Codes/Sportizza/app/views/side_nav.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/c32bb29bee.js" crossorigin="anonymous"></script>
+        <!-- <link rel="stylesheet" type="text/css" href="/public/css/root.css"> -->
+        <link rel="stylesheet" type="text/css" href="/public/css/layout.css">
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="page-container">
+            <div class="side-nav">
+                <ul class="side-nav-container">
+                    <li><a href="profile"><i class="far fa-user-circle"></i>Profile</a></li>
+                    <li><a href="profile"><i class="far fa-bookmark"></i>Add Booking</a></li>
+                    <li><a href="booking"><i class="fas fa-search"></i>Check Booking</a></li>
+                    <li><a href="#"><i class="far fa-chart-bar"></i>Reports</a></li>
+                    <li><a href="Manage_User/manage_user.html"><i class="fas fa-users"></i>Manage User</a></li>
+                    <li><a href="#"><i class="fas fa-user-cog"></i>Setting</a></li>
+                    <li><a href="#log-out"><i class="fas fa-sign-out-alt"></i>Log Out</a></li>
+                </ul>
+            </div>
+        </div>
+    </body>
+    </html>
