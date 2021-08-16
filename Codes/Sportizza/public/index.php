764e6e024bfd45a05c95cefe8499bf51877c9f88
ManethWijetunga <2019cs194@stu.ucsc.lk>
2021-08-16 09:49:33 +0530

    autoload files

--- Codes/Sportizza/public/index.php
+++ Codes/Sportizza/public/index.php
@@ -6,17 +6,15 @@
  *PHP version 7.4.12 
  */
 
+/**
+ * Composer
+ */
+require '../vendor/autoload.php';
 
 /**
- * Autoloader
+ * Twig
  */
-spl_autoload_register(function ($class) {
-    $root = dirname(__DIR__);   // get the parent directory
-    $file = $root . '/' . str_replace('\\', '/', $class) . '.php';
-    if (is_readable($file)) {
-        require $root . '/' . str_replace('\\', '/', $class) . '.php';
-    }
-});
+Twig_Autoloader::register();
 
 
 /**
