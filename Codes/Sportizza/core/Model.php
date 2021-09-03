bdf42568736c87c66c1e979ea861698658e7bd79
ManethWijetunga <2019cs194@stu.ucsc.lk>
2021-09-03 10:30:21 +0530

    routing changes

--- Codes/Sportizza/core/Model.php
+++ Codes/Sportizza/core/Model.php
@@ -34,4 +34,104 @@ abstract class Model
 
         return $db;
     }
+
+    // Insert query
+    public function writeData($tableName, $columns, $data){
+        $sql = 'INSERT INTO '.$tableName.' ('.$columns.' ) VALUES ('.$data.');';
+
+        // echo $sql;
+        $result =  $this->con->query($sql);
+        
+        if ($result === FALSE) {
+            // echo "Database Error";
+          } else {
+            return $result; 
+        }
+    }
+    public function getAllData($tableName){
+        $sql = 'SELECT * FROM '.$tableName;
+        $result =  $this->con->query($sql);
+        if ($result === FALSE) {
+            echo "No data";
+          } else {
+            return $result; 
+        }
+    }
+    public function getAllDataWhere($tableName, $column, $data){
+        $sql = 'SELECT * FROM '.$tableName.' WHERE '.$column.'="'.$data.'"';
+        $result =  $this->con->query($sql);
+
+        
+        if ($result === FALSE) {
+            echo "No data";
+          } else {
+            return $result; 
+        }
+    }
+    public function getAllDataWhereAnd($tableName, $column1, $data1,$column2, $data2){
+        $sql = 'SELECT * FROM '.$tableName.' WHERE '.$column1.'="'.$data1.'" AND '.$column2.'="'.$data2.'"';
+        $result =  $this->con->query($sql);
+        
+
+        //echo $sql;
+
+        if ($result === FALSE) {
+            echo "No data";
+          } else {
+            return $result; 
+        }
+    }
+    public function getSpecificDataWhere($columnName,$tableName, $column, $data){
+        $sql = 'SELECT '.$columnName.' FROM '.$tableName.' WHERE '.$column.'="'.$data.'"';
+        $result =  $this->con->query($sql);
+        
+        if ($result === FALSE) {
+            echo "No data";
+          } else {
+            return $result; 
+        }
+    }
+    public function updateData($tableName, $key, $keyvalue, $data) {
+        $set = '';
+        $x = 1;
+    
+        foreach($data as $name => $value) {
+            $set .= "{$name} = \"{$value}\"";
+            if($x < count($data)) {
+                $set .= ',';
+            }
+            $x++;
+        }
+    
+        $sql = "UPDATE {$tableName} SET {$set} WHERE {$key} = {$keyvalue}";
+
+        $result =  $this->con->query($sql);
+        //   echo $sql;
+        // if(!$this->con->query($sql, $data)->error()) {
+        //     return true;
+        // }
+
+    
+        return false;
+    }
+    function deleteData($tableName, $key, $keyvalue){
+        $sql = "DELETE FROM {$tableName} WHERE {$key}='{$keyvalue}'";
+        $result =  $this->con->query($sql);
+
+        if ($result === FALSE) {
+            echo "Delete Error";
+        } else {
+            return $result; 
+        }
+    }
+    public function executeSql($query){
+        // echo $query;
+        $result =  $this->con->query($query);
+        if ($result === FALSE) {
+            echo $this->con->error;
+          } else {
+            return $result; 
+        }
+    }
+    
 }
