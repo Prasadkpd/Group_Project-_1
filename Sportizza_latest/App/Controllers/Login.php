9e6246b0519c9e07b87cc992f3205cf3a563ec72
hbsbashitha <hbsbashitha@gmail.com>
2021-09-16 20:31:06 +0530

    complete login navigation

--- Sportizza_latest/App/Controllers/Login.php
+++ Sportizza_latest/App/Controllers/Login.php
@@ -27,14 +27,27 @@ class Login extends \Core\Controller
         if ($user) {
 
             // session_regenerate_id(true);
-
             // $_SESSION['user_id'] = $user->id;
+            //Redirects to home page atm (Change it to customised home page)
 
-            // //Redirects to home page atm (Change it to customised home page)
-            // $this->redirect('');
-            View::renderTemplate('Customer/new_customer_dashboard.html');
+            if($user->type=='Admin'){
+                $this->redirect('/login/adminlogin');
+            }
 
-            
+            elseif($user->type=='Customer'){
+                $this->redirect('/login/customerlogin');
+            }
+            elseif($user->type=='Manager'){
+                $this->redirect('/login/managerlogin');
+            }
+            elseif($user->type=='AdministrationStaff'){
+                $this->redirect('/login/administrationstafflogin');
+            }
+
+            elseif($user->type=='BookingHandlingStaff'){
+                $this->redirect('/login/bookinghandlingstafflogin');
+            }
+           
         } else {
 
             View::renderTemplate('LoginSignup/login.html', [
@@ -44,8 +57,39 @@ class Login extends \Core\Controller
             
         }
 
-        // echo($_REQUEST["username"]. "," . $_REQUEST["password"]);
+        
     }
+
+
+    public function adminloginAction()
+    {
+        //direct to the admin page
+        View::renderTemplate('Admin/admin-FAQ.html');
+    }
+
+    public function customerloginAction()
+    {
+        //direct to the customer page
+        View::renderTemplate('Customer/new_customer_dashboard.html');
+    }
+ 
+    public function bookinghandlingstaffloginAction()
+    {
+        //direct to the customer page
+        View::renderTemplate('BookHandelStaff/manage-bookings.html');
+    }
+    public function administrationstaffloginAction()
+    {
+        //direct to the customer page
+        View::renderTemplate('AdministrationStaff/manage-facility.html');
+    }
+
+    public function managerloginAction()
+    {
+        //direct to the customer page
+        View::renderTemplate('Customer/new_customer_dashboard.html');
+    }
+
      //Logout for a user
     // public function destroyAction()
     // {
--- Sportizza_latest/Core/Controller.php
+++ Sportizza_latest/Core/Controller.php
@@ -37,5 +37,11 @@ abstract class Controller
 
     }
 
+    public function redirect($url){
+        header('Location: http://' . $_SERVER['HTTP_HOST'] . $url , true ,303);
+        exit;
+
+    }
+
 
 }
