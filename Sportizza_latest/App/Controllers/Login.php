ac609e50bb3b7aee2c2908703bb120812ead0eeb
hbsbashitha <hbsbashitha@gmail.com>
2021-09-16 19:33:58 +0530

    login back end start

--- Sportizza_latest/App/Controllers/Login.php
+++ Sportizza_latest/App/Controllers/Login.php
@@ -3,7 +3,7 @@
 
 namespace App\Controllers;
 use Core\View;
-use \App\Models\User;
+use \App\Models\LoginModel;
 
 
 class Login extends \Core\Controller
@@ -17,48 +17,59 @@ class Login extends \Core\Controller
     //Login for a user
     public function createAction(){
     
-        $user = User::authenticate($_POST['username'], $_POST['password']);
+        // $user = LoginModel::findByUsername($_POST['username']);
 
+        // var_dump($user);
+
+
+        $user=LoginModel::authenticate($_POST['username'],$_POST['password']);
+        // echo($user);
         if ($user) {
 
-            session_regenerate_id(true);
+            // session_regenerate_id(true);
 
-            $_SESSION['user_id'] = $user->id;
+            // $_SESSION['user_id'] = $user->id;
 
-            //Redirects to home page atm (Change it to customised home page)
-            $this->redirect('/');
+            // //Redirects to home page atm (Change it to customised home page)
+            // $this->redirect('');
+            View::renderTemplate('Customer/new_customer_dashboard.html');
 
+            
         } else {
 
-            View::renderTemplate('Login/login.html', [
+            View::renderTemplate('LoginSignup/login.html', [
                 'username' => $_POST['username'],
             ]);
+
+            
         }
+
+        // echo($_REQUEST["username"]. "," . $_REQUEST["password"]);
     }
      //Logout for a user
-    public function destroyAction()
-    {
-        // Unset all of the session variables
-        $_SESSION = [];
+    // public function destroyAction()
+    // {
+    //     // Unset all of the session variables
+    //     $_SESSION = [];
 
-        // Delete the session cookie
-        if (ini_get('session.use_cookies')) {
-            $params = session_get_cookie_params();
+    //     // Delete the session cookie
+    //     if (ini_get('session.use_cookies')) {
+    //         $params = session_get_cookie_params();
 
-            setcookie(
-                session_name(),
-                '',
-                time() - 42000,
-                $params['path'],
-                $params['domain'],
-                $params['secure'],
-                $params['httponly']
-            );
-        }
+    //         setcookie(
+    //             session_name(),
+    //             '',
+    //             time() - 42000,
+    //             $params['path'],
+    //             $params['domain'],
+    //             $params['secure'],
+    //             $params['httponly']
+    //         );
+    //     }
 
-        // Finally destroy the session
-        session_destroy();
+    //     // Finally destroy the session
+    //     session_destroy();
 
-        $this->redirect('/');
-    }
+    //     $this->redirect('/');
+    // }
     }
