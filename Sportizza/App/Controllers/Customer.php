5ff2806d3defb46de827563fa19b7e0691c28c20
jonathan <jonathan10dass@gmail.com>
2021-09-27 23:16:27 +0530

    integrated payhere to cart

--- Sportizza/App/Controllers/Customer.php
+++ Sportizza/App/Controllers/Customer.php
@@ -47,5 +47,34 @@ class Customer extends Authenticated
         View::renderTemplate('Customer/customerBookingView.html');
     }
 
+    // public function paymentAction()
+    // {
+        
+    //     $merchant_id         = $_POST['merchant_id'];
+    //     $order_id             = $_POST['order_id'];
+    //     $payhere_amount     = $_POST['amount'];
+    //     $payhere_currency    = $_POST['currency'];
+    //     $status_code         = $_POST['status_code'];
+    //     $md5sig                = $_POST['md5sig'];
+    //     $method             =$_POST['method'];
+        
+    //     if ($method=='VISA'||$method=='MASTER'){
+    //         $card_holder_name = $_POST['card_holder_name'];
+    //         $card_no = $_POST['card_no'];
+    //         $card_expiry = $_POST['card_expiry'];
+    //     }
+        
+    //     // $md5sig = strtoupper (md5 ( $merchant_id . $order_id . $payhere_amount . $payhere_currency . $status_code + strtoupper(md5($payhere_secret)) ) );
+        
+    //     $merchant_secret = '4fTw237GjlF4Ob8177KghV8LWMTRKbBKu4ErpPXVXnC1'; // Replace with your Merchant Secret (Can be found on your PayHere account's Settings page)
+
+    //     $local_md5sig = strtoupper (md5 ( $merchant_id . $order_id . $payhere_amount . $payhere_currency . $status_code . strtoupper(md5($merchant_secret)) ) );
+
+    //     if (($local_md5sig === $md5sig) AND ($status_code == 2) ){
+    //             //TODO: Update your database as payment success
+    //     }
+
+    // }
+
    
 }
