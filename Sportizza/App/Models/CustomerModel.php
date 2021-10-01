477efe84d5127f6e6d6362ee95291cdb224a0fb4
ManethWijetunga <2019cs194@stu.ucsc.lk>
2021-10-01 21:53:03 +0530

    Did changes to top navbar of cart

--- Sportizza/App/Models/CustomerModel.php
+++ Sportizza/App/Models/CustomerModel.php
@@ -37,12 +37,12 @@ class CustomerModel extends \Core\Model
                 sports_arena_profile.category,time_slot.start_time,time_slot.end_time 
                 FROM booking INNER JOIN booking_timeslot ON booking.booking_id = booking_timeslot.booking_id 
                 INNER JOIN time_slot ON booking_timeslot.timeslot_id = time_slot.time_slot_id INNER JOIN sports_arena_profile 
-<<<<<<< HEAD
+
                 ON booking.sports_arena_id = sports_arena_profile.sports_arena_id WHERE booking.customer_user_id=:id
+                AND `booking`.`security_status`="active"
                 ORDER BY booking.booking_date DESC';
-=======
-                ON booking.sports_arena_id = sports_arena_profile.sports_arena_id WHERE booking.customer_user_id=:id AND `booking`.`security_status`="active"';
->>>>>>> 8eaf293f41469561618c0ff09d2ad2d098f94a64
+
+                
         
 
 
