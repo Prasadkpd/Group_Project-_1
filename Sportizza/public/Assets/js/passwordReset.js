605d3b9ac990890bac84596e3c95e18b1dd79449
ManethWijetunga <2019cs194@stu.ucsc.lk>
2021-10-04 11:07:25 +0530

    Password reset view done

---
+++ Sportizza/public/Assets/js/passwordReset.js
@@ -0,0 +1,29 @@
+
+const newPassword = document.getElementById('newPassword');
+const togglePassword = document.querySelector('#togglePassword');
+const showPassword = document.querySelector('#password');
+//Add Form
+const formUserPasswordReset = document.getElementById('formUserPasswordReset');
+
+formUserPasswordReset.addEventListener('submit', function (event) {
+    // Prevent default behaviour
+    event.preventDefault();
+    if (
+        validateNewPassword()
+    ) {
+        formUpdateTimeslot.submit();
+    }
+});
+
+function validatePasswordResetForm() {
+    validateNewPassword();
+}
+
+function validateNewPassword() {
+    if (checkIfEmptyNext(newPassword)) return;
+    if (!meetLengthNext(newPassword, 8, 255)) return;
+    regEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+    if (!matchWithRegExPassword(regEx, newPassword)) return;
+    return true;
+
+}
