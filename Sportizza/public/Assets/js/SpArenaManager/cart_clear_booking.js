968f54eaf97b7ab305b7507dfdf001da0e0a209f
jonathan <jonathan10dass@gmail.com>
2021-11-17 00:19:59 +0530

    partly done clear booking from cart 2.0

--- Sportizza/public/Assets/js/SpArenaManager/cart_clear_booking.js
+++ Sportizza/public/Assets/js/SpArenaManager/cart_clear_booking.js
@@ -5,25 +5,22 @@ $(document).ready(function () {
         console.log(booking_id);
         console.log(price);
 
-        // $.ajax({
-        //     type: "POST",
+        $.ajax({
+            type: "POST",
 
-        //     url: "http://localhost/spadministrationstaff/clearbooking/" + dateValue,
+            url: "http://localhost/spadministrationstaff/clearbooking/" + dateValue,
             
-        //     dataType: "text",
+            dataType: "text",
             
-        //     success: function (response) {
-        //         // console.log(response);
-        //         
-                    // $("#cartItem" + booking_id).hide();
-                    // console.log("Successfully cleared booking no " + booking_id);
+            success: function (response) {
+                    // console.log(response);   
+                    $("#cartItem" + booking_id).hide();
+                    console.log("Successfully cleared booking no " + booking_id);
                     let total = $("#storeTotal").val();
                     let newSum = String(total - price);
                     let displayTotal = "LKR " + newSum;
                     $("#showTotal").html(displayTotal);
-                
-                
-        //     }
-        // })
+            }
+        })
     })
 })
